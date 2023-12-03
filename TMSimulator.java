cb3e9bfbd22be170fc1151db86b8463b1943f6ee
KaiSorensen <kaisorensen@u.boisestate.edu>
2023-12-02 18:10:00 -0700

    adds formatted output

--- TMSimulator.java
+++ TMSimulator.java
@@ -9,8 +9,18 @@ public class TMSimulator {
         int numStates;
         int numSymbols;
         String transitions = "";
-        String tape = "0000000";
+        String tape = "";
 
+        //this if statement handles the arguments
+        if(args.length == 2) {
+            tape = args[1];
+        } else if (args.length == 1) {
+            tape = "0000000";
+        } else {
+            System.out.println("\nERROR: invalid number of arguments");
+        }
+        
+        //this try-catch block scans and stores the data in the file
         try {
             Scanner fileScan = new Scanner(new File(args[0]));
             numStates = fileScan.nextInt();
@@ -21,14 +31,24 @@ public class TMSimulator {
                 //Scanner lineScan = new Scanner(fileScan.nextLine());
                 transitions += fileScan.nextLine();
             }
+            fileScan.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        //here we create the Turing machine and run the simulation
+        TM sim = new TM(numStates, numSymbols, transitions, tape);
+        tape = sim.runSimulation();
 
-        // TM sim = new TM(numStates, numSymbols, transitions, tape);
-        // sim.runSimulation();
+        //here we construct the output
+        if(tape.length() > 500) tape = "very large";
+        String output = args[0] + "\n"
+                        + "output: " + tape + "\n"
+                        + "output length: " + tape.length() + "\n"
+                        + "sum of symbols: " + sim.getSum();
+         
+        System.out.println("\n" + output + "\n");
 
     }
 
